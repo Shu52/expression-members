77b7b46422978281c17569ec0f45261fc547279a
Shuaib Sajid <shusajid@gmail.com>
2018-08-27 11:18:29 -0500

    Finished exercise

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace expression_members
+{
+    public class Bug
+    {
+        /*
+            You can declare a typed public property, make it read-only,
+            and initialize it with a default value all on the same
+            line of code in C#. Readonly properties can be set in the
+            class' constructors, but not by external code.
+        */
+        public string Name { get; } = "";
+        public string Species { get; } = "";
+        public ICollection<string> Predators { get; } = new List<string>();
+        public ICollection<string> Prey { get; } = new List<string>();
+
+        // Convert this readonly property to an expression member
+        public string FormalName
+        {
+            get
+            {
+                return $"{this.Name} the {this.Species}";
+            }
+        }
+
+        // Class constructor
+        public Bug(string name, string species, List<string> predators, List<string> prey)
+        {
+            this.Name = name;
+            this.Species = species;
+            this.Predators = predators;
+            this.Prey = prey;
+        }
+
+        // Convert this method to an expression member
+        public string PreyList => string.Join(",", this.Prey);
+
+
+        // Convert this method to an expression member
+        public string PredatorList => string.Join(",", this.Predators);
+
+
+
+        // Convert this to expression method (hint: use a C# ternary)
+        public string Eat(string food) =>
+
+            (this.Prey.Contains(food)) ? $"{this.Name} ate the {food}." : $"{this.Name} is still hungry.";
+
+
+        static void Main(string[] args)
+        {
+
+            List<string> predators = new List<string>(){
+                "Birds","Bats"
+            };
+            List<string> prey = new List<string>(){
+            "Aphids","ButterFlies","Flies"
+        };
+
+            Bug Eddie = new Bug("Eddie", "PrayingMantis",predators,prey);
+         Console.WriteLine(Eddie.Eat("Aphids") + Eddie.FormalName);
+         Console.WriteLine(Eddie.PredatorList);
+         Console.WriteLine(Eddie.PreyList);
+        }
+    }
+}
